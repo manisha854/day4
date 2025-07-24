3c663bac060dbd422da9a54399dffa78ddfa0b3e
manisha854 <2311cs010243@mallareddyuniversity.ac.in>
2025-07-24 20:31:51 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,18 @@
+import java.nio.channels.Pipe.SourceChannel;
+
+public class Demo {
+    public static void main(String[] args) {
+        int nums[][]=new int[5][2];
+        for(int i=0;i<5;i++){
+            for(int j=0;j<2;j++){
+                nums[i][j]=(int) (Math.random()*10);
+    }
+}
+for(int i=0;i<5;i++){
+    for(int j = 0;j<2;j++){
+         System.out.print(nums[i][j] + " ");
+    }
+    System.out.println();
+    }
+}
+    }
